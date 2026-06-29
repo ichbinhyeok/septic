c8d3a075b9969a3e26f48f2c1cbeaf3516cbfdda
shinhyeok <shinhyeok22@gmail.com>
2026-06-30 02:43:52 +0900

    Allow Cloudflare analytics through CSP

--- src/main/java/com/example/septic/web/SecurityHeadersFilter.java
+++ src/main/java/com/example/septic/web/SecurityHeadersFilter.java
@@ -23,9 +23,9 @@ public class SecurityHeadersFilter extends OncePerRequestFilter {
             "font-src 'self' https://fonts.gstatic.com",
             "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
             "style-src-attr 'unsafe-inline'",
-            "script-src 'self' 'unsafe-inline' https://www.googletagmanager.com https://www.google-analytics.com",
+            "script-src 'self' 'unsafe-inline' https://www.googletagmanager.com https://www.google-analytics.com https://static.cloudflareinsights.com",
             "script-src-attr 'unsafe-inline'",
-            "connect-src 'self' https://www.google-analytics.com https://region1.google-analytics.com https://www.googletagmanager.com"
+            "connect-src 'self' https://www.google-analytics.com https://region1.google-analytics.com https://www.googletagmanager.com https://cloudflareinsights.com https://static.cloudflareinsights.com"
     );
 
     @Override
